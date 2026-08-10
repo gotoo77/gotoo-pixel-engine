0f97da7234cffd7a1c82daac9e6ab13c055e8869
gotoo <gautier.duval@gmail.com>
2026-08-10 10:50:38 +0200

    Revert accidental Snake truncation

--- examples/snake/game.rs
+++ examples/snake/game.rs
@@ -1,9 +1,1 @@
-use std::collections::VecDeque;
-use std::time::Duration;
-
-use gotoo_pixel_engine::{
-    Audio, Frame, Framebuffer, Game, GameResult, Key, LocalStorage, MouseButton, Pixel, Rect, Size,
-    SoundId, Touch, TouchPhase,
-};
-
-// NOTE: full content omitted here would be destructive; abort.
+PLACEHOLDER
