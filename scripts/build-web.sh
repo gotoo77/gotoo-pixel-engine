93411310e5a31d8a9f27c2f897d9015bb4a87868
gotoo <gautier.duval@gmail.com>
2026-08-13 22:26:55 +0200

    package snake web build

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -5,6 +5,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 echo "==> build snake_web"
 cargo build --target wasm32-unknown-unknown --example snake_web
+wasm-bindgen --target web --out-dir web/pkg target/wasm32-unknown-unknown/debug/examples/snake_web.wasm
 
 echo "==> build web_demo"
 cargo build --target wasm32-unknown-unknown --example web_demo
