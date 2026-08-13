fea1d7d916d6cc0e78818e89976dc5f9996ccb39
gotoo <gautier.duval@gmail.com>
2026-08-13 13:12:48 +0200

    Export gamepad calibration profiles

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod audio_wav;
 mod control;
 pub mod framebuffer;
 mod gamepad;
+mod gamepad_profile;
 mod input;
 mod pixel;
 mod platform;
@@ -14,8 +15,10 @@ pub use audio::{Audio, AudioError, NoopAudio, SoundBank, SoundId};
 pub use audio_wav::pcm16_mono_wav;
 pub use control::{ActionId, ControlBinding, ControlMap};
 pub use framebuffer::Framebuffer;
+pub use gamepad_profile::{AxisCalibration, GamepadProfile};
 pub use input::{
-    ButtonState, GamepadButton, GamepadId, Input, Key, MouseButton, Touch, TouchPhase,
+    ButtonState, GamepadButton, GamepadConnectionEvent, GamepadDeviceInfo, GamepadId, Input, Key,
+    MouseButton, Touch, TouchPhase,
 };
 pub use pixel::Pixel;
 pub use platform::{EngineConfig, EngineError, Frame, Game, GameResult, run};
