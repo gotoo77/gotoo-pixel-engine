9c0c11f85592dbff64cb7756519d797f3cd30149
gotoo <gautier.duval@gmail.com>
2026-08-15 00:10:16 +0200

    add Breakout wasm launcher

---
+++ examples/breakout_web.rs
@@ -0,0 +1,22 @@
+#[allow(dead_code)]
+#[path = "breakout.rs"]
+mod breakout;
+
+use breakout::{BreakoutApp, FRAMEBUFFER_HEIGHT, TOUCH_FRAMEBUFFER_WIDTH};
+use gotoo_pixel_engine::{EngineConfig, run};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    run(
+        EngineConfig {
+            title: "Breakout".into(),
+            framebuffer_width: TOUCH_FRAMEBUFFER_WIDTH,
+            framebuffer_height: FRAMEBUFFER_HEIGHT,
+            window_width: TOUCH_FRAMEBUFFER_WIDTH * 3,
+            window_height: FRAMEBUFFER_HEIGHT * 3,
+        },
+        BreakoutApp::new_touch(),
+    )
+    .map_err(|err| JsValue::from_str(&err.to_string()))
+}
