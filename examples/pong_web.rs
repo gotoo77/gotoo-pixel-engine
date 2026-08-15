29961771cae4709e7a612d085aa8073e23caebce
gotoo <gautier.duval@gmail.com>
2026-08-15 18:12:55 +0200

    reuse pong core from web entrypoint

--- examples/pong_web.rs
+++ examples/pong_web.rs
@@ -1,9 +1,8 @@
-#[allow(dead_code)]
-#[path = "pong.rs"]
+#[path = "pong/game.rs"]
 mod pong;
 
 use gotoo_pixel_engine::{EngineConfig, run};
-use pong::{FRAMEBUFFER_WIDTH, PongApp, TOUCH_FRAMEBUFFER_HEIGHT};
+use pong::{FRAMEBUFFER_WIDTH, PongGame, TOUCH_FRAMEBUFFER_HEIGHT};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
@@ -16,7 +15,7 @@ pub fn start() -> Result<(), JsValue> {
             window_width: FRAMEBUFFER_WIDTH * 3,
             window_height: TOUCH_FRAMEBUFFER_HEIGHT * 3,
         },
-        PongApp::new_touch(),
+        PongGame::new_touch(),
     )
     .map_err(|err| JsValue::from_str(&err.to_string()))
 }
