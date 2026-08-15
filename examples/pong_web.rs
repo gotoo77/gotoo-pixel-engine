1bfd950f0e32061e51c0d684fd74c1d61cae32b5
gotoo <gautier.duval@gmail.com>
2026-08-15 10:34:58 +0200

    move Pong touch controls below playfield

--- examples/pong_web.rs
+++ examples/pong_web.rs
@@ -3,7 +3,7 @@
 mod pong;
 
 use gotoo_pixel_engine::{EngineConfig, run};
-use pong::{FRAMEBUFFER_HEIGHT, FRAMEBUFFER_WIDTH, PongApp};
+use pong::{FRAMEBUFFER_WIDTH, PongApp, TOUCH_FRAMEBUFFER_HEIGHT};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
@@ -12,9 +12,9 @@ pub fn start() -> Result<(), JsValue> {
         EngineConfig {
             title: "Pong".into(),
             framebuffer_width: FRAMEBUFFER_WIDTH,
-            framebuffer_height: FRAMEBUFFER_HEIGHT,
+            framebuffer_height: TOUCH_FRAMEBUFFER_HEIGHT,
             window_width: FRAMEBUFFER_WIDTH * 3,
-            window_height: FRAMEBUFFER_HEIGHT * 3,
+            window_height: TOUCH_FRAMEBUFFER_HEIGHT * 3,
         },
         PongApp::new_touch(),
     )
