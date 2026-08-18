fb6480b337a76650345e83946691002041a73f85
gotoo <gautier.duval@gmail.com>
2026-08-18 18:46:12 +0200

    chore(void-canticle): scope legacy slice lints

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -1,8 +1,11 @@
 mod game {
+    #[allow(dead_code, clippy::collapsible_if, clippy::too_many_arguments)]
     include!("void_canticle/game.rs");
     include!("void_canticle/v07_visuals.rs");
+    #[allow(clippy::collapsible_if)]
     include!("void_canticle/v07_game.rs");
     include!("void_canticle/v07_weapon.rs");
+    #[allow(dead_code, clippy::approx_constant, clippy::collapsible_if)]
     include!("void_canticle/v09_game.rs");
     include!("void_canticle/v10_game.rs");
 }
