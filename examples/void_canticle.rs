fe0ff97b5d74db8481c687b6dcba77e309251414
gotoo <gautier.duval@gmail.com>
2026-08-19 00:28:00 +0200

    feat(void-canticle): launch VC1.4 mutations

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -35,6 +35,11 @@ mod game {
                             pub(crate) mod v13 {
                                 use super::*;
                                 include!("void_canticle/v13_game.rs");
+
+                                pub(crate) mod v14 {
+                                    use super::*;
+                                    include!("void_canticle/v14_game.rs");
+                                }
                             }
                         }
                     }
@@ -43,9 +48,9 @@ mod game {
         }
     }
 
-    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::run_v13_with_obs_mirror;
+    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::run_v14_with_obs_mirror;
 }
 
 fn main() -> Result<(), gotoo_pixel_engine::EngineError> {
-    game::run_v13_with_obs_mirror()
+    game::run_v14_with_obs_mirror()
 }
