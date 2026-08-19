f3e2690af92c47ce1701f0c853f743e5a0579010
gotoo <gautier.duval@gmail.com>
2026-08-19 16:38:35 +0200

    fix(void-canticle): run stabilized VC2.1 slice

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -72,6 +72,7 @@ mod game {
                                                                     use super::*;
                                                                     include!("void_canticle/v21_game.rs");
                                                                     include!("void_canticle/v21_runtime.rs");
+                                                                    include!("void_canticle/v21_stabilization.rs");
                                                                 }
                                                             }
                                                         }
@@ -89,9 +90,9 @@ mod game {
         }
     }
 
-    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::run_v21_runtime_with_obs_mirror;
+    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::run_v21_stabilized_with_obs_mirror;
 }
 
 fn main() -> Result<(), gotoo_pixel_engine::EngineError> {
-    game::run_v21_runtime_with_obs_mirror()
+    game::run_v21_stabilized_with_obs_mirror()
 }
