764c9f9c24cf746dbf0adf863b2ed8d6a98e80cd
gotoo <gautier.duval@gmail.com>
2026-08-19 08:20:25 +0200

    feat(void-canticle): launch VC1.6 Void pressure

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -43,6 +43,11 @@ mod game {
                                     pub(crate) mod v15 {
                                         use super::*;
                                         include!("void_canticle/v15_game.rs");
+
+                                        pub(crate) mod v16 {
+                                            use super::*;
+                                            include!("void_canticle/v16_game.rs");
+                                        }
                                     }
                                 }
                             }
@@ -53,9 +58,9 @@ mod game {
         }
     }
 
-    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::run_v15_with_obs_mirror;
+    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::v16::run_v16_with_obs_mirror;
 }
 
 fn main() -> Result<(), gotoo_pixel_engine::EngineError> {
-    game::run_v15_with_obs_mirror()
+    game::run_v16_with_obs_mirror()
 }
