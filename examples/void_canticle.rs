1107a4438208565202cedd74d57c6b8ba438ffbc
gotoo <gautier.duval@gmail.com>
2026-08-19 18:46:20 +0200

    feat(void-canticle): run VC2.2 movement feel slice

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -79,6 +79,7 @@ mod game {
                                                                     pub(crate) mod v22 {
                                                                         use super::*;
                                                                         include!("void_canticle/v22_game.rs");
+                                                                        include!("void_canticle/v22_movement.rs");
                                                                     }
                                                                 }
                                                             }
@@ -97,9 +98,9 @@ mod game {
         }
     }
 
-    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::run_v22_with_obs_mirror;
+    pub(crate) use legacy_base::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::run_v22_movement_with_obs_mirror;
 }
 
 fn main() -> Result<(), gotoo_pixel_engine::EngineError> {
-    game::run_v22_with_obs_mirror()
+    game::run_v22_movement_with_obs_mirror()
 }
