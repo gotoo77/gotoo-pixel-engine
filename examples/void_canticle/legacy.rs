2f4e0a4b90ebd07bdb0cba42aa7d67de661cedaf
gotoo <gautier.duval@gmail.com>
2026-08-21 00:53:29 +0200

    refactor(void-canticle): quarantine versioned legacy modules

---
+++ examples/void_canticle/legacy.rs
@@ -0,0 +1,136 @@
+#![allow(dead_code, clippy::collapsible_if, clippy::too_many_arguments)]
+
+macro_rules! legacy_include {
+    ($path:literal) => {
+        include!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/examples/void_canticle/",
+            $path
+        ));
+    };
+}
+
+legacy_include!("game.rs");
+
+pub(crate) mod v07 {
+    #![allow(dead_code, clippy::collapsible_if)]
+
+    use super::*;
+    legacy_include!("v07_visuals.rs");
+    legacy_include!("v07_game.rs");
+    legacy_include!("v07_weapon.rs");
+
+    pub(crate) mod v09 {
+        #![allow(dead_code, clippy::approx_constant, clippy::collapsible_if)]
+
+        use super::*;
+        legacy_include!("v09_game.rs");
+
+        pub(crate) mod v10 {
+            use super::*;
+            legacy_include!("v10_game.rs");
+
+            pub(crate) mod v11 {
+                #![allow(clippy::assign_op_pattern)]
+
+                use super::*;
+                legacy_include!("v11_game.rs");
+
+                pub(crate) mod v12 {
+                    use super::*;
+                    legacy_include!("v12_game.rs");
+
+                    pub(crate) mod v13 {
+                        use super::*;
+                        legacy_include!("v13_game.rs");
+
+                        pub(crate) mod v14 {
+                            use super::*;
+                            legacy_include!("v14_game.rs");
+
+                            pub(crate) mod v15 {
+                                use super::*;
+                                legacy_include!("v15_game.rs");
+
+                                pub(crate) mod v16 {
+                                    use super::*;
+                                    legacy_include!("v16_game.rs");
+
+                                    pub(crate) mod v16b {
+                                        use super::*;
+                                        legacy_include!("v16b_game.rs");
+
+                                        pub(crate) mod v17 {
+                                            use super::*;
+                                            legacy_include!("v17_game.rs");
+
+                                            pub(crate) mod v18 {
+                                                use super::*;
+                                                legacy_include!("v18_game.rs");
+
+                                                pub(crate) mod v19 {
+                                                    use super::*;
+                                                    legacy_include!("v19_game.rs");
+
+                                                    pub(crate) mod v20 {
+                                                        #![allow(unused_assignments)]
+
+                                                        use super::*;
+                                                        legacy_include!("v20_game.rs");
+
+                                                        pub(crate) mod v21 {
+                                                            use super::*;
+                                                            legacy_include!("v21_game.rs");
+                                                            legacy_include!("v21_runtime.rs");
+                                                            legacy_include!("v21_tuning.rs");
+                                                            legacy_include!("v21_stabilization.rs");
+                                                            legacy_include!("v21_survival_cleanup.rs");
+
+                                                            pub(crate) mod v22 {
+                                                                use super::*;
+                                                                legacy_include!("v22_game.rs");
+                                                                legacy_include!("v22_movement.rs");
+                                                                legacy_include!("v22_passives.rs");
+
+                                                                pub(crate) mod v23 {
+                                                                    use super::*;
+                                                                    legacy_include!("v23_game.rs");
+                                                                    legacy_include!("v23_sustain.rs");
+                                                                    legacy_include!(
+                                                                        "v23_visual_foundation.rs"
+                                                                    );
+
+                                                                    pub(crate) mod presentation {
+                                                                        use super::*;
+                                                                        legacy_include!("v27.rs");
+                                                                        legacy_include!(
+                                                                            "v27/choice_runtime.rs"
+                                                                        );
+                                                                        legacy_include!(
+                                                                            "presentation_frontend.rs"
+                                                                        );
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// This is the only boundary through which current code may reach the historical
+// implementation. The version-numbered module graph stays quarantined here.
+pub(crate) use v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::v23::presentation::run_void_canticle_with_obs_mirror;
+
+#[cfg(target_arch = "wasm32")]
+pub(crate) use v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::v23::presentation::vc27_preload_choice_catalog_web as preload_choice_catalog_web;
