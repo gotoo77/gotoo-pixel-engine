f10ca0befbdb2e600f3139e372403d4a71af154b
gotoo <gautier.duval@gmail.com>
2026-08-21 10:40:55 +0200

    refactor(void-canticle): expose semantic choice preload entrypoint

--- examples/void_canticle.rs
+++ examples/void_canticle.rs
@@ -12,7 +12,7 @@ pub(crate) mod game {
     mod current {
         pub(crate) use super::legacy::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::v23::presentation::run_void_canticle_with_obs_mirror;
         #[cfg(target_arch = "wasm32")]
-        pub(crate) use super::legacy::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::v23::presentation::vc27_preload_choice_catalog_web as preload_choice_catalog_web;
+        pub(crate) use super::legacy::v07::v09::v10::v11::v12::v13::v14::v15::v16::v16b::v17::v18::v19::v20::v21::v22::v23::presentation::preload_choice_catalog_web;
     }
 
     #[cfg(target_arch = "wasm32")]
