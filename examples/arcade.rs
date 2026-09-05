97786c3b958d330dd5958a458b54608ffa35d80d
gotoo <gautier.duval@gmail.com>
2026-09-06 00:07:00 +0200

    test(input): expose Arcade gamepad mapping capabilities

--- examples/arcade.rs
+++ examples/arcade.rs
@@ -39,7 +39,7 @@ impl Game for ArcadeWithGamepadProbe {
 
 fn draw_gamepad_probe(frame: &mut Frame<'_>) {
     let first = frame.input.gamepad_ids().next();
-    let label = if let Some(id) = first {
+    let (info_label, state_label) = if let Some(id) = first {
         let up = frame.input.gamepad_button(id, GamepadButton::DPadUp).held();
         let down = frame
             .input
@@ -56,9 +56,22 @@ fn draw_gamepad_probe(frame: &mut Frame<'_>) {
         let south = frame.input.gamepad_button(id, GamepadButton::South).held();
         let x = frame.input.gamepad_axis(id, GamepadAxis::LeftStickX);
         let y = frame.input.gamepad_axis(id, GamepadAxis::LeftStickY);
-        format!(
-            "PAD {}  D:{}{}{}{}  LS:{:+.2},{:+.2}  A:{}",
-            id.as_usize(),
+        let info = frame.input.gamepad_info(id);
+        let info_label = if let Some(info) = info {
+            format!(
+                "PAD {} {}  MAP:{:?}  CAPS A:{:?} D:{:?} LSX:{:?}",
+                id.as_usize(),
+                info.name,
+                info.mapping_source,
+                info.capabilities.button(GamepadButton::South),
+                info.capabilities.button(GamepadButton::DPadUp),
+                info.capabilities.axis(GamepadAxis::LeftStickX),
+            )
+        } else {
+            format!("PAD {} (NO DEVICE INFO)", id.as_usize())
+        };
+        let state_label = format!(
+            "STATE D:{}{}{}{}  LS:{:+.2},{:+.2}  A:{}",
             if up { 'U' } else { '-' },
             if down { 'D' } else { '-' },
             if left { 'L' } else { '-' },
@@ -66,12 +79,16 @@ fn draw_gamepad_probe(frame: &mut Frame<'_>) {
             x,
             y,
             if south { '1' } else { '0' },
-        )
+        );
+        (info_label, state_label)
     } else {
-        "PAD: NONE (GILRS DID NOT DETECT A CONTROLLER)".to_owned()
+        (
+            "PAD: NONE (GILRS DID NOT DETECT A CONTROLLER)".to_owned(),
+            String::new(),
+        )
     };
 
-    let height = 14_u32.min(frame.framebuffer.height());
+    let height = 26_u32.min(frame.framebuffer.height());
     let y = i32::try_from(frame.framebuffer.height().saturating_sub(height)).unwrap_or(0);
     let bounds = Rect {
         x: 0,
@@ -88,11 +105,30 @@ fn draw_gamepad_probe(frame: &mut Frame<'_>) {
     );
     draw_text_centered(
         frame.framebuffer,
-        bounds,
-        &label,
+        Rect {
+            x: bounds.x,
+            y: bounds.y,
+            width: bounds.width,
+            height: 12,
+        },
+        &info_label,
         1,
         Pixel::rgb(111, 238, 184),
     );
+    if !state_label.is_empty() {
+        draw_text_centered(
+            frame.framebuffer,
+            Rect {
+                x: bounds.x,
+                y: bounds.y.saturating_add(13),
+                width: bounds.width,
+                height: 12,
+            },
+            &state_label,
+            1,
+            Pixel::rgb(230, 238, 235),
+        );
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
